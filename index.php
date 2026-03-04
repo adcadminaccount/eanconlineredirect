7ec1badab30d97d2cbf339f1512d7f4e9764d1c5
adcadminaccount <109055480+adcadminaccount@users.noreply.github.com>
2026-03-04 14:55:56 +1300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php header("Location: https://dwnl.screenbeam.com/resource/conference/windows/x86/ScreenBeam_Conference_Windows_1.0.5.29.zip"); ?>
+<?php header("Location: https://eanetworkscentre.jonasleisure.co.nz"); ?>
